51f231215846f23bcc2227c47b59b910ea201a59
Qasoskor12262008 <javapro@gmail.com>
2025-08-11 21:15:52 +0500

    tugadi

---
+++ js/main.js
@@ -0,0 +1,441 @@
+const btnall = document.getElementById("btn-all");
+const btnshavarma = document.getElementById("btn-shavarma");
+const btnturk = document.getElementById("btn-turk");
+const btnburder = document.getElementById("btn-burger");
+const btndoner = document.getElementById("btn-doner");
+const btnsushi = document.getElementById("btn-sushi");
+const btnpizza = document.getElementById("btn-pizza");
+const btndesert = document.getElementById("btn-desert");
+
+const desert = document.getElementById("desert")
+const turk = document.getElementById("turk")
+const shavarma = document.getElementById("shavarma")
+const pizza = document.getElementById("pizza")
+const doner = document.getElementById("doner")
+const burger = document.getElementById("burger")
+const sushi = document.getElementById("sushi")
+const desert2 = document.getElementById("desert2")
+const turk2 = document.getElementById("turk2")
+const shavarma2 = document.getElementById("shavarma2")
+const sushi2 = document.getElementById("sushi2")
+const doner2 = document.getElementById("doner2")
+
+
+btnall.addEventListener("click" , function(){
+    desert.classList.remove("netu")
+})
+btnall.addEventListener("click" , function(){
+    turk.classList.remove("netu")
+})
+btnall.addEventListener("click" , function(){
+    shavarma.classList.remove("netu")
+})
+btnall.addEventListener("click" , function(){
+    pizza.classList.remove("netu")
+})
+btnall.addEventListener("click" , function(){
+    doner.classList.remove("netu")
+})
+btnall.addEventListener("click" , function(){
+    burger.classList.remove("netu")
+})
+btnall.addEventListener("click" , function(){
+    sushi.classList.remove("netu")
+})
+btnall.addEventListener("click" , function(){
+    desert2.classList.remove("netu")
+})
+btnall.addEventListener("click" , function(){
+    turk2.classList.remove("netu")
+})
+btnall.addEventListener("click" , function(){
+    shavarma2.classList.remove("netu")
+})
+btnall.addEventListener("click" , function(){
+    sushi2.classList.remove("netu")
+})
+btnall.addEventListener("click" , function(){
+    doner2.classList.remove("netu")
+})
+
+
+btnshavarma.addEventListener("click" , function(){
+    shavarma.classList.remove("netu")
+})
+btnshavarma.addEventListener("click" , function(){
+    shavarma2.classList.remove("netu")
+})
+btnshavarma.addEventListener("click" , function(){
+    turk.classList.add("netu")
+})
+btnshavarma.addEventListener("click" , function(){
+    desert.classList.add("netu")
+})
+btnshavarma.addEventListener("click" , function(){
+    pizza.classList.add("netu")
+})
+btnshavarma.addEventListener("click" , function(){
+    doner.classList.add("netu")
+})
+btnshavarma.addEventListener("click" , function(){
+    burger.classList.add("netu")
+})
+btnshavarma.addEventListener("click" , function(){
+    sushi.classList.add("netu")
+})
+btnshavarma.addEventListener("click" , function(){
+    turk2.classList.add("netu")
+})
+btnshavarma.addEventListener("click" , function(){
+    desert2.classList.add("netu")
+})
+btnshavarma.addEventListener("click" , function(){
+    doner2.classList.add("netu")
+})
+btnshavarma.addEventListener("click" , function(){
+    sushi2.classList.add("netu")
+})
+
+
+
+btnturk.addEventListener("click" , function(){
+    turk.classList.remove("netu")
+})
+btnturk.addEventListener("click" , function(){
+    turk2.classList.remove("netu")
+})
+btnturk.addEventListener("click" , function(){
+    desert.classList.add("netu")
+})
+btnturk.addEventListener("click" , function(){
+    desert2.classList.add("netu")
+})
+btnturk.addEventListener("click" , function(){
+    shavarma.classList.add("netu")
+})
+btnturk.addEventListener("click" , function(){
+    shavarma2.classList.add("netu")
+})
+btnturk.addEventListener("click" , function(){
+    sushi.classList.add("netu")
+})
+btnturk.addEventListener("click" , function(){
+    sushi2.classList.add("netu")
+})
+btnturk.addEventListener("click" , function(){
+    doner.classList.add("netu")
+})
+btnturk.addEventListener("click" , function(){
+    doner2.classList.add("netu")
+})
+btnturk.addEventListener("click" , function(){
+    burger.classList.add("netu")
+})
+btnturk.addEventListener("click" , function(){
+    pizza.classList.add("netu")
+})
+
+
+
+
+btnburder.addEventListener("click" , function(){
+    burger.classList.remove("netu")
+})
+btnburder.addEventListener("click" , function(){
+    desert.classList.add("netu")
+})
+btnburder.addEventListener("click" , function(){
+    desert2.classList.add("netu")
+})
+btnburder.addEventListener("click" , function(){
+    sushi.classList.add("netu")
+})
+btnburder.addEventListener("click" , function(){
+    sushi2.classList.add("netu")
+})
+btnburder.addEventListener("click" , function(){
+    turk.classList.add("netu")
+})
+btnburder.addEventListener("click" , function(){
+    turk2.classList.add("netu")
+})
+btnburder.addEventListener("click" , function(){
+    shavarma.classList.add("netu")
+})
+btnburder.addEventListener("click" , function(){
+    shavarma2.classList.add("netu")
+})
+btnburder.addEventListener("click" , function(){
+    doner.classList.add("netu")
+})
+btnburder.addEventListener("click" , function(){
+    doner2.classList.add("netu")
+})
+btnburder.addEventListener("click" , function(){
+    pizza.classList.add("netu")
+})
+
+
+
+btndoner.addEventListener("click" , function(){
+    doner.classList.remove("netu")
+})
+btndoner.addEventListener("click" , function(){
+    doner2.classList.remove("netu")
+})
+btndoner.addEventListener("click" , function(){
+    desert.classList.add("netu")
+})
+btndoner.addEventListener("click" , function(){
+    desert2.classList.add("netu")
+})
+btndoner.addEventListener("click" , function(){
+    turk.classList.add("netu")
+})
+btndoner.addEventListener("click" , function(){
+    turk2.classList.add("netu")
+})
+btndoner.addEventListener("click" , function(){
+    shavarma2.classList.add("netu")
+})
+btndoner.addEventListener("click" , function(){
+    shavarma.classList.add("netu")
+})
+btndoner.addEventListener("click" , function(){
+    sushi2.classList.add("netu")
+})
+btndoner.addEventListener("click" , function(){
+    sushi.classList.add("netu")
+})
+btndoner.addEventListener("click" , function(){
+    burger.classList.add("netu")
+})
+btndoner.addEventListener("click" , function(){
+    pizza.classList.add("netu")
+})
+
+
+
+btnsushi.addEventListener("click" , function(){
+    sushi.classList.remove("netu")
+})
+btnsushi.addEventListener("click" , function(){
+    sushi2.classList.remove("netu")
+})
+btnsushi.addEventListener("click" , function(){
+    desert.classList.add("netu")
+})
+btnsushi.addEventListener("click" , function(){
+    desert2.classList.add("netu")
+})
+btnsushi.addEventListener("click" , function(){
+    turk.classList.add("netu")
+})
+btnsushi.addEventListener("click" , function(){
+    turk2.classList.add("netu")
+})
+btnsushi.addEventListener("click" , function(){
+    shavarma2.classList.add("netu")
+})
+btnsushi.addEventListener("click" , function(){
+    shavarma.classList.add("netu")
+})
+btnsushi.addEventListener("click" , function(){
+    doner2.classList.add("netu")
+})
+btnsushi.addEventListener("click" , function(){
+    doner.classList.add("netu")
+})
+btnsushi.addEventListener("click" , function(){
+    burger.classList.add("netu")
+})
+btnsushi.addEventListener("click" , function(){
+    pizza.classList.add("netu")
+})
+
+
+
+btnpizza.addEventListener("click" , function(){
+    pizza.classList.remove("netu")
+})
+btnpizza.addEventListener("click" , function(){
+    doner2.classList.add("netu")
+})
+btnpizza.addEventListener("click" , function(){
+    desert.classList.add("netu")
+})
+btnpizza.addEventListener("click" , function(){
+    desert2.classList.add("netu")
+})
+btnpizza.addEventListener("click" , function(){
+    turk.classList.add("netu")
+})
+btnpizza.addEventListener("click" , function(){
+    turk2.classList.add("netu")
+})
+btnpizza.addEventListener("click" , function(){
+    shavarma2.classList.add("netu")
+})
+btnpizza.addEventListener("click" , function(){
+    shavarma.classList.add("netu")
+})
+btnpizza.addEventListener("click" , function(){
+    sushi2.classList.add("netu")
+})
+btnpizza.addEventListener("click" , function(){
+    sushi.classList.add("netu")
+})
+btnpizza.addEventListener("click" , function(){
+    burger.classList.add("netu")
+})
+btnpizza.addEventListener("click" , function(){
+    doner.classList.add("netu")
+})
+
+
+
+btndesert.addEventListener("click" , function(){
+    desert.classList.remove("netu")
+})
+btndesert.addEventListener("click" , function(){
+    desert2.classList.remove("netu")
+})
+btndesert.addEventListener("click" , function(){
+    doner.classList.add("netu")
+})
+btndesert.addEventListener("click" , function(){
+    doner2.classList.add("netu")
+})
+btndesert.addEventListener("click" , function(){
+    turk.classList.add("netu")
+})
+btndesert.addEventListener("click" , function(){
+    turk2.classList.add("netu")
+})
+btndesert.addEventListener("click" , function(){
+    shavarma2.classList.add("netu")
+})
+btndesert.addEventListener("click" , function(){
+    shavarma.classList.add("netu")
+})
+btndesert.addEventListener("click" , function(){
+    sushi2.classList.add("netu")
+})
+btndesert.addEventListener("click" , function(){
+    sushi.classList.add("netu")
+})
+btndesert.addEventListener("click" , function(){
+    burger.classList.add("netu")
+})
+btndesert.addEventListener("click" , function(){
+    pizza.classList.add("netu")
+})
+
+
+
+
+
+const showbtn = document.getElementById("showbtn");
+const hidebtn = document.getElementById("hidebtn")
+
+showbtn.addEventListener("click" , function(){
+    burger.classList.add("shaw")
+})
+showbtn.addEventListener("click" , function(){
+    sushi2.classList.add("shaw")
+})
+showbtn.addEventListener("click" , function(){
+    doner2.classList.add("shaw")
+})
+showbtn.addEventListener("click" , function(){
+    doner.classList.add("shaw")
+})
+showbtn.addEventListener("click" , function(){
+    desert2.classList.add("shaw")
+})
+showbtn.addEventListener("click" , function(){
+    shavarma2.classList.add("shaw")
+})
+
+
+hidebtn.addEventListener("click" , function(){
+    burger.classList.remove("shaw")
+})
+hidebtn.addEventListener("click" , function(){
+    sushi2.classList.remove("shaw")
+})
+hidebtn.addEventListener("click" , function(){
+    doner2.classList.remove("shaw")
+})
+hidebtn.addEventListener("click" , function(){
+    doner.classList.remove("shaw")
+})
+hidebtn.addEventListener("click" , function(){
+    desert2.classList.remove("shaw")
+})
+hidebtn.addEventListener("click" , function(){
+    shavarma2.classList.remove("shaw")
+})
+
+
+
+const menutog = document.getElementById("menutog")
+const menuhide = document.getElementById("menuhide")
+const tellist = document.getElementById("tellist")
+
+menutog.addEventListener("click" , function(){
+    tellist.classList.add("spawn")
+})
+menuhide.addEventListener("click" , function(){
+    tellist.classList.remove("spawn")
+})
+
+
+const mangal1 = document.getElementById("mangal1")
+const mangal2 = document.getElementById("mangal2")
+const mangal3 = document.getElementById("mangal3")
+const mangal4 = document.getElementById("mangal4")
+const mangal5 = document.getElementById("mangal5")
+const mangal6 = document.getElementById("mangal6")
+const mangalcome = document.getElementById("mangalcome")
+const mangalhide = document.getElementById("mangalhide")
+
+
+mangalcome.addEventListener("click" , function(){
+    mangal1.classList.add("show")
+})
+mangalcome.addEventListener("click" , function(){
+    mangal2.classList.add("show")
+})
+mangalcome.addEventListener("click" , function(){
+    mangal3.classList.add("show")
+})
+mangalcome.addEventListener("click" , function(){
+    mangal4.classList.add("show")
+})
+mangalcome.addEventListener("click" , function(){
+    mangal5.classList.add("show")
+})
+mangalcome.addEventListener("click" , function(){
+    mangal6.classList.add("show")
+})
+
+
+mangalhide.addEventListener("click" , function(){
+    mangal1.classList.remove("show")
+})
+mangalhide.addEventListener("click" , function(){
+    mangal2.classList.remove("show")
+})
+mangalhide.addEventListener("click" , function(){
+    mangal3.classList.remove("show")
+})
+mangalhide.addEventListener("click" , function(){
+    mangal4.classList.remove("show")
+})
+mangalhide.addEventListener("click" , function(){
+    mangal5.classList.remove("show")
+})
+mangalhide.addEventListener("click" , function(){
+    mangal6.classList.remove("show")
+})
